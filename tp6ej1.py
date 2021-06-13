a5cce8459cdf29d93389da9a3921cd12698937cc
Mauri <mauriboye@gmail.com>
2021-06-13 18:23:52 -0300

    Elimine los espacios y agregue la función marco
    El ejercicio me parece muy bueno, elimine los espacios para poder hacer el código mas legible, también cambie las asignaciones "+=" a "= variable + 1" por una recomendación del profe. Añadí la función marco para la visualización de la consola, también añadí docstring en las funciones. En la excepción agregue un print con un contador de errores pero no estoy seguro de si es lo que se pedía.

--- tp6ej1.py
+++ tp6ej1.py
@@ -1,4 +1,3 @@
-
 ################
 # Nohtyp
 # Mauricio Boyé - @MauriBoye
@@ -6,71 +5,54 @@
 # Ivo Spezzacatena - @ivospezza
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
-
-
-# Reemplazar por las funciones del ejercicio
+"""
+Ejercicio N°1
+"""
 
 def anagrama(cadena1, cadena2):
-    
+    """
+    Esta función indica si dos cadenas son anagramas
+    """
     sin_espacios = cadena1.replace(" ", "")
-    
     sin_espacios = sin_espacios.lower()
-    
     lista1 = list(sin_espacios)
-    
     sin_espacios = cadena2.replace(" ", "")
-    
     sin_espacios = sin_espacios.lower()
-    
     lista2 = list(sin_espacios)
-    
     cantidad_de_coincidencias = 0
-    
-    cantidad_de_letras = 0 
-    
+    cantidad_de_letras = 0
+    errores = 0
     for letra_2 in lista2:
-        
-        cantidad_de_letras += 1
-    
+        cantidad_de_letras = cantidad_de_letras + 1
     for letra in lista1:
-        
         try:       
-            
             lista2.index(letra)
             lista2.remove(letra)
-
-
-            cantidad_de_coincidencias += 1
-            
+            cantidad_de_coincidencias = cantidad_de_coincidencias + 1
         except ValueError:
-            
-            pass
-            
-    if cantidad_de_coincidencias == cantidad_de_letras:
-        
-        return True
-    
-    else:
-        
-        return False
-                           
+            errores = errores + 1
+            print(f"--> Cantidad de errores: {errores}")
+    return cantidad_de_coincidencias == cantidad_de_letras
+
+def marco(texto):
+    """
+    Esta funcion crea un marco a un texto deseado
+    """
+    print("\n"+"╴"*80)
+    print("╔"+"═"*len(texto)+"╗")
+    print(f"║{texto}║")
+    print("╚"+"═"*len(texto)+"╝")
+    print("╴"*80)
 
 def principal():
-    
-    palabra1 = input("ingrese 1 palabra \n")
-    
-    palabra2 = input("ingrese una segunda palabra \n")
-    
+    marco("anagrama()")
+    palabra1 = input("Ingrese una palabra: ")
+    palabra2 = input("Ingrese otra palabra: ")
     son_anagrama = anagrama(palabra1, palabra2)
-    
     if son_anagrama:
-        
-        print(f"'{palabra1}' y '{palabra2}' son anagramas")
-        
+        print(f"\n'{palabra1}' y '{palabra2}' son anagramas")
     else:
-        
-        print(f"'{palabra1}' y '{palabra2}' NO son anagramas")
-        
+        print(f"\n'{palabra1}' y '{palabra2}' NO son anagramas")
 
 if __name__ == "__main__":
     principal()
