cbba80f2a2db098bb3d68a99004454f08eb9be85
IvoSpezza <83659212+IvoSpezza@users.noreply.github.com>
2021-06-18 12:41:22 -0300

    corrección, ahora descube anagramas correctamente

--- Ejercicios/tp6ej1.py
+++ Ejercicios/tp6ej1.py
@@ -13,25 +13,63 @@ def anagrama(cadena1, cadena2):
     """
     Esta función indica si dos cadenas son anagramas
     """
-    sin_espacios = cadena1.replace(" ", "")
-    sin_espacios = sin_espacios.lower()
-    lista1 = list(sin_espacios)
-    sin_espacios = cadena2.replace(" ", "")
-    sin_espacios = sin_espacios.lower()
-    lista2 = list(sin_espacios)
+    
+    palabra1 = cadena1
+    
+    palabra2 = cadena2
+    
+    if not cadena1.isalpha():
+        
+        depuracion = list(cadena1)
+        
+        for caracter in depuracion:
+            
+            if not caracter.isalpha():
+                
+                palabra1 = palabra1.replace(caracter,"")
+    
+    if not cadena2.isalpha():
+        
+        depuracion = list(cadena2)
+        
+        for caracter in depuracion:
+            
+            if not caracter.isalpha():
+                
+                palabra2 = palabra2.replace(caracter,"")
+    
+    todo_minuscula = palabra1.lower()
+    
+    lista1 = list(todo_minuscula)
+    
+    todo_minuscula = palabra2.lower()
+    
+    lista2 = list(todo_minuscula)
+    
+    
     cantidad_de_coincidencias = 0
     cantidad_de_letras = 0
-    errores = 0
+    
+    
     for letra_2 in lista2:
         cantidad_de_letras = cantidad_de_letras + 1
+        
+        
     for letra in lista1:
-        try:       
+        
+        try:
+            
             lista2.index(letra)
+            
             lista2.remove(letra)
+            
             cantidad_de_coincidencias = cantidad_de_coincidencias + 1
-        except ValueError:
-            errores = errores + 1
-            print(f"--> Cantidad de errores: {errores}")
+            
+        except ValueError: # este value error esta solo para atajar el error de "lista2.index" y seguir con el programa
+                           # porque cuando una palabra no es anagrama con otra tiraria error, y eso no es error, es que no
+                           # son anagrama
+            pass
+            
     return cantidad_de_coincidencias == cantidad_de_letras
 
 def marco(texto):
@@ -45,11 +83,17 @@ def marco(texto):
     print("╴"*80)
 
 def principal():
+    
     marco("anagrama()")
+    
     palabra1 = input("Ingrese una palabra: ")
+    
     palabra2 = input("Ingrese otra palabra: ")
+    
     son_anagrama = anagrama(palabra1, palabra2)
+    
     if son_anagrama:
+        
         print(f"\n'{palabra1}' y '{palabra2}' son anagramas")
     else:
         print(f"\n'{palabra1}' y '{palabra2}' NO son anagramas")
