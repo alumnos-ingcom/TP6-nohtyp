dd61ab3d0682fe8457f33a0cddfb8f30884e7857
IvoSpezza <83659212+IvoSpezza@users.noreply.github.com>
2021-06-18 10:21:45 -0300

    todas las plantillas para cada ejercicio

---
+++ Ejercicios/tp6ej2.py
@@ -0,0 +1,19 @@
+################
+# Nohtyp
+# Mauricio Boyé - @MauriBoye
+# Facundo Bistevins - @FacuBiste
+# Ivo Spezzacatena - @ivospezza
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+# Reemplazar por las funciones del ejercicio
+
+
+def principal():
+    """Toda la interacción con el usuario va acá"""
+    pass
+
+if __name__ == "__main__":
+    principal()
+
