ed224a0a23242d972047b4b17f2b270072a5d75b
IvoSpezza <83659212+IvoSpezza@users.noreply.github.com>
2021-06-18 12:41:41 -0300

    ejercicio 2 listo para revisión

--- Ejercicios/tp6ej2.py
+++ Ejercicios/tp6ej2.py
@@ -8,11 +8,63 @@
 
 
 # Reemplazar por las funciones del ejercicio
+from tp6ej1 import anagrama
 
-
+def palabras_de_anagrama(texto):
+    
+    dos_palabras = texto.readline()
+    
+    lista_de_palabras = dos_palabras.split() 
+    
+    palabra1 = []
+    
+    palabra2 = []
+    
+    que_palabra = True
+    
+    for palabra in lista_de_palabras:
+        
+        if que_palabra:
+            
+            if palabra == "–":
+            
+                que_palabra = False
+            
+            else:
+                palabra1.append(palabra)
+        else:
+            palabra2.append(palabra)
+            
+    palabra1 = " ".join(palabra1)
+    
+    palabra2 = " ".join(palabra2)
+    
+    return palabra1, palabra2
+    
 def principal():
-    """Toda la interacción con el usuario va acá"""
-    pass
+    
+    texto = open("anagramas.txt", "r+", 1, "utf-8")
+    
+    while True:
+        
+        palabra1, palabra2 = palabras_de_anagrama(texto)
+        
+        if palabra1 == "":
+            
+            break
+        
+        else:
+            
+            son_anagrama = anagrama(palabra1, palabra2)
+        
+        if son_anagrama:
+            
+            print(f"'{palabra1}' y '{palabra2}' son anagrama \n\n")
+            
+        else:
+            
+            print(f"'{palabra1}' y '{palabra2}' NO son anagrama\n\n")
+            
 
 if __name__ == "__main__":
     principal()
