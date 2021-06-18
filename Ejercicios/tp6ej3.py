24a6f122f0ab4d45c18c4589f0f2da2a0778ccd6
Mauri <mauriboye@gmail.com>
2021-06-18 17:56:27 -0300

    Primera versión ejercicio N°3
    Ademas del tercer ejercicio, adjunto 2 archivos para utilizar a manera de prueba con el código

--- Ejercicios/tp6ej3.py
+++ Ejercicios/tp6ej3.py
@@ -5,15 +5,26 @@
 # Ivo Spezzacatena - @ivospezza
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
+"""
+Ejercicio N°3
+"""
+import tp6ej1
 
-
-# Reemplazar por las funciones del ejercicio
-
+def copiadora(texto_1, texto_2):
+    """
+    Esta función copia el contenido de un archivo de entrada en otro
+    archivo de salida
+    """
+    with open(texto_1, "r", 1, "utf-8") as entrada:
+        with open(texto_2, "w", 1, "utf-8") as salida:
+            copia = entrada.read()
+            salida.write(copia)
 
 def principal():
-    """Toda la interacción con el usuario va acá"""
-    pass
+    tp6ej1.marco("copiadora()")
+    texto_1 = input("Ingrese el nombre del archivo de entrada: ")
+    texto_2 = input("Ingrese el nombre del archivo de salida: ")
+    copiadora(texto_1, texto_2)
 
 if __name__ == "__main__":
     principal()
-
