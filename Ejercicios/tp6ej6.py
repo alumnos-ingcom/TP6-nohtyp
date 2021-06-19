0feda7a8b9b478fd8aa8c51e52bb88a816d7ad9c
Mauri <mauriboye@gmail.com>
2021-06-19 16:21:42 -0300

    Primera versión ejercicio N°6

--- Ejercicios/tp6ej6.py
+++ Ejercicios/tp6ej6.py
@@ -5,15 +5,61 @@
 # Ivo Spezzacatena - @ivospezza
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
+"""
+Ejercicio N°6
+"""
+import tp6ej1
 
+def hace_etiqueta(contenido, etiqueta):
+    '''
+    Retorna el contenido envuelto en una etiqueta como está indicado en el segundo argumento.
+    La funcion llamada con contenido="Hola" y etiqueta="h1" retornará
+    <h1>Hola</h1>
+    '''
+    etiqueta = "<" + etiqueta + ">" + contenido + "</" + etiqueta + ">"
+    return etiqueta
+    
+def hace_comenta(contenido):
+    '''
+    Retorna el contenido envuelto en las marcas de comentario HTML
+    <!--  -->
+    (separen las marcas del contenido con un espacio)
+    '''
+    comentario = "<!-- " + contenido + " -->"
+    return comentario
 
-# Reemplazar por las funciones del ejercicio
-
+def archivo_html(nombre):
+    '''
+    Crea un archivo html con un nombre ingresado por el usuario
+    '''
+    nombre_final = nombre + ".html"
+    encabezado1 = hace_etiqueta('Hola HTML', 'h1')
+    encabezado2 = hace_etiqueta('Hola HTML', 'h2')
+    encabezado3 = hace_etiqueta('Hola HTML', 'h3')
+    encabezado4 = hace_etiqueta('Hola HTML', 'h4')
+    encabezado5 = hace_etiqueta('Hola HTML', 'h5')
+    parrafo = hace_etiqueta('Mama estoy en internet', 'p')
+    with open(nombre_final, "x", 1, "utf-8") as archivo:
+        archivo.write("<html>\n"
+                      "    <body>\n"
+                     f"        {encabezado1}\n"
+                     f"        {encabezado2}\n"
+                     f"        {encabezado3}\n"
+                     f"        {encabezado4}\n"
+                     f"        {encabezado5}\n"
+                     f"        {parrafo}\n"
+                      "        <img src=https://i.pinimg.com/originals/c4/7d/88/c47d88963c6fd217884893c33bdc661d.gif alt=Mi imagen de prueba>\n"                      
+                      "    </body>\n"
+                      "    <footer>\n"
+                      "        © Copyright 2021 INGCOM-UNRN/nohtyp\n"
+                      "    </footer>\n"
+                      "</html>")
 
 def principal():
-    """Toda la interacción con el usuario va acá"""
-    pass
-
+    tp6ej1.marco("archivo_html()")
+    nombre = input("Ingrese el nombre del archivo html: ")
+    archivo_html(nombre)
+    print(f"El nombre del nuevo archivo es: '{nombre}.html'")
+    
 if __name__ == "__main__":
     principal()
-
